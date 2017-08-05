64a41e1db20a11be48707ea7522179c7c176d26a
Thomas Fox <fox@seitenbau.de>
2017-08-05 07:03:32 +0200

    add SecondMomentOfAreaCalculator

---
+++ src/main/java/com/github/thomasfox/wingcalculator/calculate/impl/SecondMomentOfAreaCalculator.java
@@ -0,0 +1,29 @@
+package com.github.thomasfox.wingcalculator.calculate.impl;
+
+import java.util.Map;
+
+import com.github.thomasfox.wingcalculator.calculate.Calculator;
+import com.github.thomasfox.wingcalculator.calculate.PhysicalQuantity;
+
+/**
+ * Gibt die Durchbiegung eines Flügels, das an einem Ende fest gelagert
+ * und am anderen Ende mit einer Kraft Quer zum Flügel belastet wird, zurück.
+ * Siehe Joos, Lehrbuch der Theoretischen Physik, 15. Auflage, S. 169
+ */
+public class SecondMomentOfAreaCalculator extends Calculator
+{
+
+  protected SecondMomentOfAreaCalculator()
+  {
+    super(PhysicalQuantity.WING_DEPTH,
+        PhysicalQuantity.NORMALIZED_SECOND_MOMENT_OF_AREA);
+  }
+
+  @Override
+  protected double calculateWithoutChecks(Map<PhysicalQuantity, Double> input)
+  {
+    double wingDepth = getValueOf(PhysicalQuantity.WING_DEPTH, input);
+    double mormalizedMoment = getValueOf(PhysicalQuantity.NORMALIZED_SECOND_MOMENT_OF_AREA, input);
+    return mormalizedMoment*wingDepth*wingDepth*wingDepth*wingDepth;
+  }
+}
