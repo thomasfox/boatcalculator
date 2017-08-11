7afb6a521a3d640a5770aa8523b9b2fd7aca4c01
Thomas Fox <fox@seitenbau.de>
2017-08-11 03:00:57 +0200

    add new Calculators

---
+++ src/main/java/com/github/thomasfox/wingcalculator/calculate/impl/ProfileDragCalculator.java
@@ -0,0 +1,36 @@
+package com.github.thomasfox.wingcalculator.calculate.impl;
+
+import java.util.Map;
+
+import com.github.thomasfox.wingcalculator.calculate.Calculator;
+import com.github.thomasfox.wingcalculator.calculate.PhysicalQuantity;
+
+/**
+ * https://de.wikipedia.org/wiki/Dynamischer_Auftrieb
+ */
+public class ProfileDragCalculator extends Calculator
+{
+  public ProfileDragCalculator()
+  {
+    super(PhysicalQuantity.PROFILE_DRAG,
+        PhysicalQuantity.PROFILE_DRAG_COEFFICIENT,
+        PhysicalQuantity.WING_WIDTH,
+        PhysicalQuantity.WING_DEPTH,
+        PhysicalQuantity.WING_VELOCITY,
+        PhysicalQuantity.DENSITY);
+  }
+
+  @Override
+  protected double calculateWithoutChecks(Map<PhysicalQuantity, Double> input)
+  {
+    double dragCoefficient = PhysicalQuantity.PROFILE_DRAG_COEFFICIENT.getValueFromAvailableQuantities(input);
+    double wingWidth = PhysicalQuantity.WING_WIDTH.getValueFromAvailableQuantities(input);
+    double wingDepth = PhysicalQuantity.WING_DEPTH.getValueFromAvailableQuantities(input);
+    double velocity = PhysicalQuantity.WING_VELOCITY.getValueFromAvailableQuantities(input);
+    double density = PhysicalQuantity.DENSITY.getValueFromAvailableQuantities(input);
+
+    double area = wingWidth * wingDepth;
+    double result = dragCoefficient * velocity * velocity * density * area / 2;
+    return result;
+  }
+}
