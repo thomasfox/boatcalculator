458cf63e6c426cdf55f6cf00dfe36abd57c74095
Thomas Fox <fox@seitenbau.de>
2017-08-12 07:21:57 +0200

    add toString method to SinpleXYPoint

--- src/main/java/com/github/thomasfox/wingcalculator/interpolate/SimpleXYPoint.java
+++ src/main/java/com/github/thomasfox/wingcalculator/interpolate/SimpleXYPoint.java
@@ -2,9 +2,11 @@ package com.github.thomasfox.wingcalculator.interpolate;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
 @AllArgsConstructor
+@ToString
 public class SimpleXYPoint implements XYPoint
 {
   private final double x;
