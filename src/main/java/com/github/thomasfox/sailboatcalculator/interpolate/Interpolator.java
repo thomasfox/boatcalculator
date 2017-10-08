42b7ba45e8fe056d7c6ade896b246eaec1b53096
Thomas Fox <fox@seitenbau.de>
2017-10-08 22:46:52 +0200

    separate interval matching from interpolation in interpolator

--- src/main/java/com/github/thomasfox/sailboatcalculator/interpolate/Interpolator.java
+++ src/main/java/com/github/thomasfox/sailboatcalculator/interpolate/Interpolator.java
@@ -1,6 +1,10 @@
 package com.github.thomasfox.sailboatcalculator.interpolate;
 
 import java.util.List;
+import java.util.function.Function;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 /**
  * Interpolates a quantity from some known points of a relation between x and y.
@@ -32,45 +36,89 @@ public class Interpolator
     {
       throw new InterpolatorException("The list of known values is empty");
     }
-
-    XYPoint lastValue = null;
-    double minEncounteredX = knownValues.get(0).getX();
-    double maxEncounteredX = minEncounteredX;
-
-    for (XYPoint currentValue : knownValues)
+    TwoValues<XYPoint> enclosingPoints = getEnclosing(xValue, knownValues, XYPoint::getX);
+    if (enclosingPoints.value1 == enclosingPoints.value2)
     {
-      if (currentValue == null)
+      return enclosingPoints.value1.getY();
+    }
+
+    double relativeWeightOfCurrentValue = (xValue - enclosingPoints.value2.getX()) / (enclosingPoints.value1.getX() - enclosingPoints.value2.getX());
+    double result = relativeWeightOfCurrentValue * enclosingPoints.value1.getY() + (1d - relativeWeightOfCurrentValue) * enclosingPoints.value2.getY();
+    return result;
+  }
+
+  /**
+   * Gets, from a list of discrete values, the first two neighbouring values
+   * which mapped representatives enclose a given number.
+   * If a mapped representative exactly equals the given number,
+   * then the matching value is returned twice.
+   *
+   * @param toEnclose the value to be enclosed between two numbers.
+   * @param availableValues the available values for enclosement.
+   * @param mapper the mapper which maps the avaliable number to a double,
+   *        which then is used to enclose the toEnclose value.
+   *
+   * @return the first neighbouring value which, when mapped by the mapper,
+   *         enclose the given number.
+   */
+  public <T> TwoValues<T> getEnclosing(double toEnclose, List<T> availableValues, Function<T, Double> mapper)
+  {
+    if (availableValues == null)
+    {
+      throw new InterpolatorException("The list of available values is null");
+    }
+    if (availableValues.isEmpty())
+    {
+      throw new InterpolatorException("The list of available values is empty");
+    }
+
+    T lastValue = null;
+    Double mappedLastValue = null;
+    Double minEncounteredValue = mapper.apply(availableValues.get(0));
+    Double maxEncounteredValue = minEncounteredValue;
+
+    for (T currentValue : availableValues)
+    {
+      Double mappedCurrentValue = mapper.apply(currentValue);
+      if (mappedCurrentValue == null)
       {
-        throw new InterpolatorException("The list of known values contains null");
+        throw new InterpolatorException("The list of avaliable values contains null");
       }
-      if (xValue == currentValue.getX())
+      if (toEnclose == mappedCurrentValue)
       {
-        return currentValue.getY();
+        return new TwoValues<T>(currentValue, currentValue);
       }
 
-      minEncounteredX = Math.min(currentValue.getX(), minEncounteredX);
-      maxEncounteredX = Math.max(currentValue.getX(), maxEncounteredX);
+      minEncounteredValue = Math.min(mappedCurrentValue, minEncounteredValue);
+      maxEncounteredValue = Math.max(mappedCurrentValue, maxEncounteredValue);
 
       if (lastValue == null)
       {
         lastValue = currentValue;
+        mappedLastValue = mappedCurrentValue;
         continue;
       }
 
-      Double currentMinX = Math.min(currentValue.getX(), lastValue.getX());
-      Double currentMaxX = Math.max(currentValue.getX(), lastValue.getX());
-      if (xValue < currentMinX || xValue > currentMaxX)
+      Double currentMinX = Math.min(mappedCurrentValue, mappedLastValue);
+      Double currentMaxX = Math.max(mappedCurrentValue, mappedLastValue);
+      if (toEnclose < currentMinX || toEnclose > currentMaxX)
       {
         lastValue = currentValue;
+        mappedLastValue = mappedCurrentValue;
         continue;
       }
-
-      double relativeWeightOfCurrentValue = (xValue - lastValue.getX()) / (currentValue.getX() - lastValue.getX());
-      double result = relativeWeightOfCurrentValue * currentValue.getY() + (1d - relativeWeightOfCurrentValue) * lastValue.getY();
-      return result;
+      return new TwoValues<T>(currentValue, lastValue);
     }
-    throw new InterpolatorException("The provided xValue, " + xValue
+    throw new InterpolatorException("The provided value, " + toEnclose
         + ", does not match the interpolation interval ["
-        + minEncounteredX + "," + maxEncounteredX + "]");
+        + minEncounteredValue + "," + maxEncounteredValue + "]");
+  }
+
+  @Data
+  @AllArgsConstructor
+  public static class TwoValues<T>
+  {
+    final T value1;
+    final T value2;
   }
 }
